26ce11144eda8debfe53718e3e053cc143af7d5b
pythaac <pythaac@gmail.com>
2025-02-14 22:22:32 +0900

    logout 메서드 추가

--- src/main/java/com/spring/security/controller/UserApiController.java
+++ src/main/java/com/spring/security/controller/UserApiController.java
@@ -2,8 +2,13 @@ package com.spring.security.controller;
 
 import com.spring.security.dto.AddUserRequest;
 import com.spring.security.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -14,6 +19,12 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest addUserRequest) {
         userService.save(addUserRequest);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
